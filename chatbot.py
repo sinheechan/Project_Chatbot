636006c5275a11a3618a693ddd00040ac2b2e267
Sinheechan <147711497+sinheechan@users.noreply.github.com>
2024-03-29 13:02:56 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,16 +4,13 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer #sentence_transformer 설치 필요
 from sklearn.metrics.pairwise import cosine_similarity
 import json
-# pip install numpy==1.23.0
 
-@st.cache_resource
-#@st.cache(allow_output_mutation=True) # 한 번만 로드해서 사용
+@ st.cache_resource
 def cached_model():
     model = SentenceTransformer('jhgan/ko-sroberta-multitask')
     return model
 
 @st.cache_resource
-#@st.st.cache(allow_output_mutation=True)
 def get_dataset():
     df = pd.read_csv('wellness_dataset.csv')
     df['embedding'] = df['embedding'].apply(json.loads)
@@ -25,13 +22,16 @@ df = get_dataset()
 st.header('심리상담 챗봇')
 st.markdown("[코리아 IT 아카데미], 이석창 강사)")
 
+# 대화한 내용 저장
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = [] # 대화한 내용 저장
+    st.session_state['generated'] = []
 
+# 지난 대화 저장
 if 'past' not in st.session_state:
-    st.session_state['past'] = [] # 지난 대화 저장
+    st.session_state['past'] = []
 
-with st.form('form', clear_on_submit=True): # 사용자 입력 폼
+# 사용자 입력 폼
+with st.form('form', clear_on_submit=True):
     user_input = st.text_input('당신: ', '')
     submitted = st.form_submit_button('전송')
 
